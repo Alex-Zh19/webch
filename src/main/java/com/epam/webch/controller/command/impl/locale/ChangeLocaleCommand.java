2a7aa3883953cdcd0791f197289123b90caf0c4b
Alexandr Zhalnerchik <alexzhalnerchikz@mail.ru>
2021-08-02 17:43:30 +0300

    change locale reworked

--- src/main/java/com/epam/webch/controller/command/impl/locale/ChangeLocaleCommand.java
+++ src/main/java/com/epam/webch/controller/command/impl/locale/ChangeLocaleCommand.java
@@ -3,7 +3,6 @@ package com.epam.webch.controller.command.impl.locale;
 import com.epam.webch.controller.PagePath;
 import com.epam.webch.controller.SessionAttribute;
 import com.epam.webch.controller.command.Command;
-import com.epam.webch.controller.command.CommandFactory;
 import com.epam.webch.controller.command.CommandName;
 import com.epam.webch.controller.command.Router;
 import com.epam.webch.controller.impl.RequestParameter;
@@ -18,21 +17,30 @@ public class ChangeLocaleCommand implements Command {
     public Router execute(HttpServletRequest request, HttpServletResponse response) {
         PagePath path = (PagePath) request.getSession().getAttribute(SessionAttribute.PREVIOUS_PAGE.name());
         Router router;
-        if (path == PagePath.SHOP_PAGE) {
-            router = new Router(path.getValue(), Router.RouterType.FORWARD,CommandName.OPEN_SHOP_PAGE);
-        } else if(path == PagePath.ADMIN_ENTITY_LIST_PAGE){
-            router = new Router(path.getValue(), Router.RouterType.FORWARD,CommandName.OPEN_HOME_PAGE);
-        }else if(path == PagePath.ADMIN_PRODUCT_LIST_PAGE){
-            router = new Router(path.getValue(), Router.RouterType.FORWARD,CommandName.OPEN_PRODUCTS_LIST);
-        }else if(path == PagePath.ADMIN_EMPLOYEE_ORDER_LIST_PAGE){
-            router = new Router(path.getValue(), Router.RouterType.FORWARD,CommandName.OPEN_ORDERS_LIST);
-        }else if(path == PagePath.HOME_ADMIN_PAGE||path == PagePath.HOME_EMPLOYEE_PAGE||
-                path == PagePath.HOME_USER_PAGE||path == PagePath.HOME_PAGE){
-            router = new Router(path.getValue(), Router.RouterType.FORWARD,CommandName.OPEN_HOME_PAGE);
-        }else if(path == PagePath.GUEST_CART_PAGE||path == PagePath.USER_CART_PAGE){
-            router = new Router(path.getValue(), Router.RouterType.FORWARD,CommandName.OPEN_CART_PAGE);
-        }else {
-            router = new Router(path.getValue(), Router.RouterType.FORWARD);
+        switch (path) {
+            case SHOP_PAGE:
+                router = new Router(path.getValue(), Router.RouterType.FORWARD, CommandName.OPEN_SHOP_PAGE);
+                break;
+            case ADMIN_PRODUCT_LIST_PAGE:
+                router = new Router(path.getValue(), Router.RouterType.FORWARD, CommandName.OPEN_PRODUCTS_LIST);
+                break;
+            case ADMIN_EMPLOYEE_ORDER_LIST_PAGE:
+                router = new Router(path.getValue(), Router.RouterType.FORWARD, CommandName.OPEN_ORDERS_LIST);
+                break;
+            case ADMIN_ENTITY_LIST_PAGE:
+            case HOME_ADMIN_PAGE:
+            case HOME_EMPLOYEE_PAGE:
+            case HOME_USER_PAGE:
+            case HOME_PAGE:
+                router = new Router(path.getValue(), Router.RouterType.FORWARD, CommandName.OPEN_HOME_PAGE);
+                break;
+            case GUEST_CART_PAGE:
+            case USER_CART_PAGE:
+                router = new Router(path.getValue(), Router.RouterType.FORWARD, CommandName.OPEN_CART_PAGE);
+                break;
+            default:
+                router = new Router(path.getValue(), Router.RouterType.FORWARD);
+                break;
         }
         String requestedLocale = request.getParameter(RequestParameter.LANGUAGE.getValue());
         LocaleAttribute localeAttribute = LocaleAttribute.defineLocale(requestedLocale);
